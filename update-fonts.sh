ec7bfcc9e217fbaf66a3121fc3c991775ef99890
Kirill Vercetti <office@kyzima-spb.com>
2026-01-15 01:46:43 +0300

    Add JetBrainsMono

--- update-fonts.sh
+++ update-fonts.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
-cd "$SCRIPT_DIR"
+FONTS_DIR="$HOME/.local/share/fonts"
+#SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+
+mkdir -p "$FONTS_DIR"
+cd "$FONTS_DIR"
 
 wget -O JetBrainsMono.zip https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip
-unzip -d JetBrainsMono -o JetBrainsMono.zip
+unzip -o JetBrainsMono.zip
 rm JetBrainsMono.zip
 fc-cache -fv
 
