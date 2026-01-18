efed7b60027733d1379be120ffd13491b96e8621
Kirill Vercetti <office@kyzima-spb.com>
2026-01-18 15:39:25 +0300

    Add fonts consola, droid sans, tahoma

--- scripts/update-fonts.sh
+++ scripts/update-fonts.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 FONTS_DIR="$HOME/.local/share/fonts"
-#SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
 mkdir -p "$FONTS_DIR"
 cd "$FONTS_DIR"
 
+sudo cp -r "$SCRIPT_DIR"/fonts/* /usr/local/share/fonts/
+
 wget -O JetBrainsMono.zip https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.zip
 unzip -o JetBrainsMono.zip
 rm JetBrainsMono.zip
+
 fc-cache -fv
 
 exit 0
